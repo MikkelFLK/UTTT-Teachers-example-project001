e7b0ff613bd8d83fec8b673ba4e85e796327cd05
langehk <Rupkri32@gmail.com>
2018-03-05 10:31:15 +0100

    123

--- src/dk/easv/bll/bot/MMEFBot.java
+++ src/dk/easv/bll/bot/MMEFBot.java
@@ -3,13 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package dk.easv.bll.bot;
+
+import dk.easv.bll.game.IGameState;
+import dk.easv.bll.move.IMove;
 
 /**
  *
  * @author MikkelK
  */
-public class MMEFBot
+
+public class MMEFBot implements IBot
 {
-    
-}
+    private static final String BOTNAME ="MMEF";
+
+    @Override
+    public IMove doMove(IGameState state)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getBotName()
+    {
+       
+   
+        return BOTNAME;
+    }
+    }
+
+
